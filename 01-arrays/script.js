a42b60631a7b2b87d5dacea4d7f73f855e0b26d0
William05112 <liamscervino@gmail.com>
2025-03-26 23:46:58 +0100

    feat: log

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -9,58 +9,60 @@ for (let i = 0; i < teachers.length; i++) {
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
 const fourthTeacher = teachers[3];
-//console.log(fourthTeacher);
+console.log(fourthTeacher);
 
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
 teachers[4] = 'Patrick';
-//console.log(teachers);
+console.log(teachers);
 
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
 const lastTeacher = teachers.pop(teachers.length - 1);
-//console.log(lastTeacher);
+console.log(lastTeacher);
 
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
 const firstTeacher = teachers.shift();
-//console.log(firstTeacher);
+console.log(firstTeacher);
 //console.log(teachers);
 
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
 teachers.push('Vanessa');
-//console.log(teachers);
+console.log(teachers);
 
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
 teachers.unshift('Sarah');
-//console.log(teachers);
+console.log(teachers);
 
 
 // 7. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
 const isFabioPresent = teachers.includes('Fabio');
-//console.log(isFabioPresent);
+console.log(isFabioPresent);
 
 
 // 8. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 let lewisfind = teachers.indexOf('Lewis');
-//console.log(lewisfind);
+console.log(lewisfind);
 
 
 // e salvalo nella variabile lewisIndex
 const lewisIndex = lewisfind;
+console.log(lewisIndex);
+
 
 
 // 9. Unisci tutti gli insegnanti nell'array teachers in una stringa
 let teachersstringa = teachers.join();
-//console.log(teachersstringa);
+console.log(teachersstringa);
 
 
 // separata da virgole e salvala nella variabile teachersString
 const teachersString = teachersstringa;
-//console.log(teachersString);
+console.log(teachersString);
 
 
 // 10. Verifica se l'array teachers è vuoto
